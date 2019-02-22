7406d61652c9eb511993c230f22947391c42d04d
gavin vaske <gavin@vantacollective.com>
2019-02-22 00:08:55 -0600

    Fixed bug preventing post request to be made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const webhook = require('./webhook')
 
 const app = express()
-const webhookUrl = "https://maker.ifttt.com/trigger/\"email_processed_by_server\"/with/key/b9OelmXvPgNPq5yi71DRnA"
+const webhookUrl = "https://maker.ifttt.com/trigger/email_processed_by_server/with/key/b9OelmXvPgNPq5yi71DRnA"
 
 app.get('/', function(req,res){
     res.send("Hello Mars!")
