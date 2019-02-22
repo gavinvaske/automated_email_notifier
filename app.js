cd3fd22c215271bd913a776b005c20bd2d829069
gavin vaske <gavin@vantacollective.com>
2019-02-21 22:43:12 -0600

    fixing bug

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const webhookUrl = "https://maker.ifttt.com/trigger/\"email_processed_by_server\"/with/key/b9OelmXvPgNPq5yi71DRnA"
 
 app.get('/', function(req,res){
-    res.send(webhook.postRequest('hello!'))
+    res.send("Hello Mars!")
 })
 
 app.post('/processEmail', function(req, res){
