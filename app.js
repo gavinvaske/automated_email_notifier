d57fe90fc7a2224907632405b673d68d468d896b
gavin vaske <gavin@vantacollective.com>
2019-02-21 21:08:44 -0600

    Updating project so it can run on heroku

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get('/', function(req,res){
     res.send('Hello World!')
 })
 
-let port = 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(port, function(){
-    console.log(`Server started on port ${port}...`)
+app.listen(PORT, function(){
+    console.log(`Server started on port ${PORT}...`)
 } )
