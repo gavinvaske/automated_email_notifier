d1e15877ed33951d92cb47182063a0630d11f1c7
gavin vaske <gavin@vantacollective.com>
2019-02-23 17:38:18 -0600

    Error handling and comments added

--- app.js
+++ app.js
@@ -2,47 +2,48 @@
 const express = require('express')
 const app = express()
 const webhook = require('./webhook')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+
+// Middleware
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-// Middleware
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
-
-// The url the processed email should be sent to 
+// Webhook endpoints
 const createCalendarEventUrl = "https://hooks.zapier.com/hooks/catch/4556328/pzhoz7/"
 const fineNotificationUrl = "https://hooks.zapier.com/hooks/catch/4556328/pzqz64/"
 
+// Text response when GET request is made to homepage
 app.get('/', function(req,res){
     res.send("Welcome! I am a webserver created using Node.js and Express.js!")
 })
 
+// Process data sent to this URL via POST request
 app.post('/processEmail', function(req, res){
-    // console.log('req.body:', req.body)
-    // console.log("bookTitle: ", req.body.bookTitle)
-    // console.log("dateDue: ", req.body.dateDue)
-    // console.log("timeDue: ", req.body.timeDue)
-    // console.log("timeDue: ", req.body.outstandingFine)
 
     if(typeof req.body.outstandingFine != "undefined"){
         let outstandingFine = parseFloat(req.body.outstandingFine)
         if(outstandingFine > 0){
             webhook.createFineAlert(fineNotificationUrl, '$' + outstandingFine.toString())
         }
-    }
+    } else { console.log("Undefined parameter => (outstandingFine): ", req.body.outstandingFine)}
+
     if(typeof req.body.bookTitle == "undefined" || req.body.dateDue == "undefined" || req.body.timeDue == "undefined"){
-        console.log(req.body)
+        console.log("Error, undefined parameters: ", req.body)
         return "Error, undefined parameters";
     }
+
     webhook.createCalendarEvent(createCalendarEventUrl, req)
     res.end("Success!")
 })
 
+// Use the webserver's port OR use a default port
 const PORT = process.env.PORT || 3000
 
+// Start the server 
 app.listen(PORT, function(){
     console.log(`Server started on port ${PORT}...`)
 } )
--- webhook.js
+++ webhook.js
@@ -1,5 +1,15 @@
 const request = require('request')
 
+/**
+ * Make a post request to a url to trigger the creation of a new google calendar event
+ *
+ * @param {string} url - The webhook url a post request is sent to
+ * @param {object} req - The (key) => (value) pairs used in the POST request
+ * @param {string} req.timeDue - The time of day the rental is due
+ * @param {string} req.body.bookTitle - The book name
+ * @param {string} req.body.dateDue - The date the rental is due 
+ * @return None
+ */
 module.exports.createCalendarEvent = function(url, req){
     return request.post(
         url,
@@ -14,16 +24,21 @@ module.exports.createCalendarEvent = function(url, req){
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log("SUCCESS:", body)
-                return 1;
             }
             if (error){
                 console.log('ERROR:', error);
-                return 0;
             }
         }
     );
 }
 
+/**
+ * Make a post request to a url to trigger a SMS notification
+ *
+ * @param {string} url - The webhook url a post request is sent to
+ * @param {string} outstandingFine - The dollar amount of the outstanding fine
+ * @return None
+ */
 module.exports.createFineAlert = function(url, outstandingFine){
     return request.post(
         url,
